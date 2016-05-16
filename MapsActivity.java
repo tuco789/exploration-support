e9a3a5e49b098a0a3f88cca9bbd078b9da38431b
tuco789 <svaittin@yahoo.com>
2016-05-16 18:45:05 +0300

    Draws the views with the first three UI styles, one after another Tests the consistency of arrays

--- MapsActivity.java
+++ MapsActivity.java
@@ -5,10 +5,10 @@ import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.ImageView;
 
-import fi.aalto.ming.uitestscreens.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,6 +27,7 @@ import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "fi.aalto.ming.MapsActivity";
     private static final LatLng HELSINKI = new LatLng(60.160999, 24.944800);
     private static final LatLng NEAR_HKI =
             new LatLng(HELSINKI.latitude, HELSINKI.longitude - 0.0001);
@@ -34,13 +35,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             new LatLng(HELSINKI.latitude, HELSINKI.longitude - 0.016059);
     private static final LatLng SOUTH =
             new LatLng(HELSINKI.latitude - 0.008, HELSINKI.longitude);
+
     private GoogleMap mMap;
     private final List<BitmapDescriptor> mImages = new ArrayList<BitmapDescriptor>();
     private UiSettings mUiSettings;
 
     private GroundOverlay mGroundOverlay;
+    ImageView legend;
 
     private int mCurrentEntry = 0;
+
+    private int uiStyle = 0;
 
     private TypedArray latArray;
     private TypedArray lngArray;
@@ -50,7 +55,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private int markerStart = 0;
     private int markerCount;
     private int screenCount = 0;
-//    private int markersPerScreenCount = 0;
+    private boolean arraysRecycled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,9 +83,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Sets the map type to be "none"
-        mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-
         // Disable controls
         mUiSettings = mMap.getUiSettings();
         mUiSettings.setScrollGesturesEnabled(false);
@@ -89,46 +91,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mUiSettings.setRotateGesturesEnabled(false);
         mUiSettings.setCompassEnabled(false);
 
-        // Add a marker in Punavuori, Helsinki and move the camera
-
-        /*        LatLng DeliCafeMaya = new LatLng(60.1616273, 24.9390844);
-        LatLng Kaivopuisto = new LatLng(60.1568098, 24.9567318);
-        LatLng MikaelAgr = new LatLng(60.1583393, 24.939403);
-        LatLng Observatory = new LatLng(60.1622016, 24.9516979);
-        LatLng Ursa = new LatLng(60.155325, 24.955435);
-        LatLng Mannerheim = new LatLng(60.1588078, 24.9601059);
-        LatLng Olympia = new LatLng(60.1607413, 24.9566449);
-*/
-/*      mMap.addMarker(new MarkerOptions().position(DeliCafeMaya).title("Deli Café Maya")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_cafe_20)));
-        mMap.addMarker(new MarkerOptions().position(Kaivopuisto).title("Kaivopuisto")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_park_20)));
-        mMap.addMarker(new MarkerOptions().position(MikaelAgr).title("Mikael Agricola")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_church_20)));
-        mMap.addMarker(new MarkerOptions().position(Observatory).title("Observatory")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_culture_20)));
-        mMap.addMarker(new MarkerOptions().position(Ursa).title("Ursa")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_viewpoint_20)));
-        mMap.addMarker(new MarkerOptions().position(Mannerheim).title("Mannerheim Museum")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_museum_20)));
-        mMap.addMarker(new MarkerOptions().position(Olympia).title("Olympia Terminal")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_harbor_20)));
-
-        mMap.addMarker(new MarkerOptions().position(DeliCafeMaya).title("Deli Café Maya")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_eating_20)));
-        mMap.addMarker(new MarkerOptions().position(Kaivopuisto).title("Kaivopuisto")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_outdoor_20)));
-        mMap.addMarker(new MarkerOptions().position(MikaelAgr).title("Mikael Agricola")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_culture_20)));
-        mMap.addMarker(new MarkerOptions().position(Observatory).title("Observatory")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_culture_20)));
-        mMap.addMarker(new MarkerOptions().position(Ursa).title("Ursa")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_outdoor_20)));
-        mMap.addMarker(new MarkerOptions().position(Mannerheim).title("Mannerheim Museum")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_culture_20)));
-        mMap.addMarker(new MarkerOptions().position(Olympia).title("Olympia Terminal")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_outdoor_20))); */
-
+        // Move the camera
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(NEAR_HKI)           // Sets the center of the map to fit the UI overlay on screen
                 .zoom(14)                   // Sets the zoom to fit the UI overlay on screen
@@ -136,6 +99,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .build();                   // Creates a CameraPosition from the builder
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
+        // Read the information about configuring the screens from resource file
         Resources res = getResources();
         latArray = res.obtainTypedArray(R.array.lat);
         lngArray = res.obtainTypedArray(R.array.lng);
@@ -143,15 +107,45 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         conf_markers = res.getIntArray(R.array.conf_markers);
 
         //Test consistency of array lengths
-        //MISSING
+        int totalMarkers = 0;
+        int totalColors = 0;
 
+        for (int i=0; i<conf_colors.length; i++){
+            totalColors = totalColors+conf_colors[i];
+        }
+        if (totalColors != conf_markers.length)
+            Log.e(TAG,"conf_markers length mismatch");
+
+        for (int i=0; i<conf_markers.length; i++){
+            totalMarkers = totalMarkers+conf_markers[i];
+        }
+        if (totalMarkers != latArray.length())
+            Log.e(TAG,"latArray length mismatch");
+        if (totalMarkers != lngArray.length())
+            Log.e(TAG,"latArray length mismatch");
+
+        // Draw the first UI screen
+        legend = (ImageView)findViewById(R.id.imageView);
+        legend.performClick();
     }
 
     public void drawMarkers(View view) {
 
-        mMap.clear();
         if (screenCount<conf_colors.length) { // For each screen
+            mMap.clear();
             int markersPerScreen = 0;
+
+            // If UI style requires, sets the map type to be "none"
+            switch (uiStyle) {
+                case 0: case 1: {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                    break;
+                }
+                case 2: {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    break;
+                }
+            }
 
             // Count the number markers to be drawn for this screen
             for (int i = colorCount; i < (colorCount + conf_colors[screenCount]); i++) {
@@ -164,47 +158,69 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 float lat = (float) (HELSINKI.latitude - 0.008 + (0.016 * latArray.getFloat(markerCount, 1)));
                 float lng = (float) (HELSINKI.longitude - 0.016059 + (0.032118 * lngArray.getFloat(markerCount, 1)));
 
+                switch (uiStyle) {
 
-                // Crashes if this is included
-//            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + i)
-//                    .icon(BitmapDescriptorFactory.fromResource(typeArray.getResourceId(i, -1))));
-// or                    .icon(BitmapDescriptorFactory.fromResource(getResources().getIdentifier(typeArray.getString(i), null, getPackageName()))));
+                    case 0: { // Use dots as markers
+                        // Draw markers with the appropriate color i.e. check from conf_color if there was 2nd and 3rd color for this screen and from conf_markers how many markers to print with each color
+                        if (markerCount < (markerStart + conf_markers[colorCount])) // If should use the first color
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + markerCount)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_eating_20)));
+                        else if (conf_colors[screenCount] > 1 && markerCount < markerStart + conf_markers[colorCount] + conf_markers[colorCount + 1]) // If should use the second color
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + markerCount)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_culture_20)));
+                        else if (conf_colors[screenCount] > 2 && markerCount < markerStart + conf_markers[colorCount] + conf_markers[colorCount + 1] + conf_markers[colorCount + 2]) // If should use the third color
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + markerCount)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_outdoors_20)));
+                    break;
+                    }
 
-                // Draw markers with the appropriate color i.e. check from conf_color if there was 2nd and 3rd color for this screen and from conf_markers how many markers to print with each color
-                if (markerCount < (markerStart + conf_markers[colorCount])) // If should use the first color
-                    mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + markerCount)
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_eating_20)));
-                else if (conf_colors[screenCount] > 1 && markerCount < markerStart + conf_markers[colorCount] + conf_markers[colorCount+1]) // If should use the second color
-                    mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + markerCount)
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_culture_20)));
-                else if (conf_colors[screenCount] > 2 && markerCount < markerStart + conf_markers[colorCount] + conf_markers[colorCount+1] + conf_markers[colorCount+2]) // If should use the third color
-                    mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + markerCount)
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot_outdoor_20)));
+                    case 1: case 2: { // Use icons as markers
+                        // Draw markers with the appropriate color i.e. check from conf_color if there was 2nd and 3rd color for this screen and from conf_markers how many markers to print with each color
+                        if (markerCount < (markerStart + conf_markers[colorCount])) // If should use the first color
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + markerCount)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_eating_20)));
+                        else if (conf_colors[screenCount] > 1 && markerCount < markerStart + conf_markers[colorCount] + conf_markers[colorCount + 1]) // If should use the second color
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + markerCount)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_culture_20)));
+                        else if (conf_colors[screenCount] > 2 && markerCount < markerStart + conf_markers[colorCount] + conf_markers[colorCount + 1] + conf_markers[colorCount + 2]) // If should use the third color
+                            mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker " + markerCount)
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_outdoors_20)));
+                    break;
+                    }
+                }
+
             }
             markerStart = markerStart + markersPerScreen;
-//            markersPerScreenCount = markersPerScreenCount + markersPerScreen;
             colorCount = colorCount + conf_colors[screenCount];
             screenCount++;
+        } else if (uiStyle < 3) {
+            // Switch to new UI style and start over for next screen
+            uiStyle++;
+            markerStart = 0;
+            colorCount = 0;
+            screenCount = 0;
+        } else if (!arraysRecycled) { // If drawn all the screens for all UI styles, recycle the resources
+            latArray.recycle();
+            lngArray.recycle();
+            arraysRecycled = true;
         }
 
         mMap.addMarker(new MarkerOptions().position(HELSINKI).title("Your location"));
         mMap.addMarker(new MarkerOptions().position(WEST).title("West"));
         mMap.addMarker(new MarkerOptions().position(SOUTH).title("South"));
 
-        mImages.clear();
-        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.radar_sq));
+        // If UI style requires, adds a radar UI overlay at Punavuori, Helsinki
+        if (uiStyle != 2) {
+            mImages.clear();
+            mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.radar_sq));
+            mGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+                    .image(mImages.get(mCurrentEntry)).anchor(0.495f, 0.5f)
+                    .position(HELSINKI, 1969f, 1969f)
+                    .bearing(180));
+        }
 
-        // Add a radar UI overlay at Punavuori, Helsinki.
-        mGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
-                .image(mImages.get(mCurrentEntry)).anchor(0.495f, 0.5f)
-                .position(HELSINKI, 1969f, 1969f)
-                .bearing(180));
+        if (screenCount == 0)
+            legend.performClick();
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();  // Always call the superclass method first
-        latArray.recycle();
-        lngArray.recycle();
-    }
 }
